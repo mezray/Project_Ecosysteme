10fe422c0f4c31566f4c6c834bcd4bfd635df926
Zhao Yiming <zhao.yiming423@gmail.comm>
2021-12-14 22:39:07 +0100

    delete ALL "if"

--- Entite/animauxC.py
+++ Entite/animauxC.py
@@ -13,18 +13,9 @@ class loup(Carnivore):
     
     #sprite du bled
     def draw(self, position, i):
-        if i == 0 or i == 4:
-            image = pygame.image.load("./sprite/pug/frame-1.gif")
-            screen.blit(image, position)
-        if i == 1 or i == 5:
-            image = pygame.image.load("./sprite/pug/frame-2.gif")
-            screen.blit(image, position)
-        if i == 2 or i == 6:
-            image = pygame.image.load("./sprite/pug/frame-3.gif")
-            screen.blit(image, position)
-        if i == 3 or i == 7:
-            image = pygame.image.load("./sprite/pug/frame-4.gif")
-            screen.blit(image, position)
+        gifNumber='./sprite/pug/frame-'+str(i%4+1)+'.gif'
+        image = pygame.image.load(gifNumber)
+        screen.blit(image, position)
 
 
 class chat(Carnivore):
@@ -37,30 +28,9 @@ class chat(Carnivore):
         self.attaque = self.energie
         
     def draw(self, position, i):
-        if i == 0:
-            image = pygame.image.load("./sprite/cat/frame-1.gif")
-            screen.blit(image, position)
-        if i == 1:
-            image = pygame.image.load("./sprite/cat/frame-2.gif")
-            screen.blit(image, position)
-        if i == 2:
-            image = pygame.image.load("./sprite/cat/frame-3.gif")
-            screen.blit(image, position)
-        if i == 3:
-            image = pygame.image.load("./sprite/cat/frame-4.gif")
-            screen.blit(image, position)
-        if i == 4:
-            image = pygame.image.load("./sprite/cat/frame-5.gif")
-            screen.blit(image, position)
-        if i == 5:
-            image = pygame.image.load("./sprite/cat/frame-6.gif")
-            screen.blit(image, position)
-        if i == 6:
-            image = pygame.image.load("./sprite/cat/frame-7.gif")
-            screen.blit(image, position)
-        if i == 7:
-            image = pygame.image.load("./sprite/cat/frame-8.gif")
-            screen.blit(image, position)
+        gifNumber='./sprite/cat/frame-'+str(i+1)+'.gif'
+        image = pygame.image.load(gifNumber)
+        screen.blit(image, position)
 
 p=0
 while p < 1:
