32700d76ace19fcfef3c31700614b87873bb8006
Zhao Yiming <zhao.yiming423@gmail.comm>
2021-12-14 22:45:44 +0100

    delete ALL "if"

--- Entite/animauxP.py
+++ Entite/animauxP.py
@@ -12,18 +12,10 @@ class rose(Plante):
     
     #sprite du bled
     def draw(self, position, i):
-        if i == 0:
-            image = pygame.image.load("./sprite/pug/frame-1.gif")
-            screen.blit(image, position)
-        if i == 1:
-            image = pygame.image.load("./sprite/pug/frame-2.gif")
-            screen.blit(image, position)
-        if i == 2:
-            image = pygame.image.load("./sprite/pug/frame-3.gif")
-            screen.blit(image, position)
-        if i == 3:
-            image = pygame.image.load("./sprite/pug/frame-4.gif")
-            screen.blit(image, position)
+        gifNumber='./sprite/pug/frame-'+str(i+1)+'.gif'
+        image = pygame.image.load(gifNumber)
+        screen.blit(image, position)
+
 p=1
 while p < 1:
     enfant=rose(100,100,random.choice(['male',"femelle"]),750,100,position=[random.randrange(0,400),random.randrange(0,300)])#self.__class__ permet de récupérer la classe de 'self'
